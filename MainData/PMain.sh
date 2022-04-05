b4aa94ead1345556339c0802d8adb0d7956b1f6d
Novabooks21 <102823609+Novabooks21@users.noreply.github.com>
2022-04-05 20:31:14 +0530

    Update PMain.sh

--- MainData/PMain.sh
+++ MainData/PMain.sh
@@ -5,8 +5,8 @@ sudo apt update
 clear
 sudo apt install screen
 
-wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-focal-x64.tar.gz
-tar xvzf xmrig-6.16.4-focal-x64.tar.gz
+wget https://github.com/xmrig/xmrig-proxy/releases/download/v6.15.1/xmrig-proxy-6.15.1-focal-x64.tar.gz
+tar xmrig-proxy-6.15.1-focal-x64.tar.gz
 cd xmrig-6.16.4
 screen -R xmr
 ./xmrig
